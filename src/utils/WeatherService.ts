14374fcca789606f83fdc84fafaadf85089fcfff
FNYukii <forestgreen14689@gmail.com>
2024-07-01 17:51:23 +0900

    feat: 東京の湿度も読み取って表示できた

--- src/utils/WeatherService.ts
+++ src/utils/WeatherService.ts
@@ -7,21 +7,28 @@ import WeatherInfo from "../entities/WeatherInfo"
 class WeatherService {
 
 
-	static async readTokyoTemp(): Promise<number | null> {
+
+	static async readTokyoWeather(): Promise<{temp: number, humi: number} | null> {
 
 		try {
 
 			// APIを呼び出す。今回は無料&登録不要の天気API「Open Meteo」を利用
-			const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m&timezone=Asia%2FTokyo&forecast_days=1')
+			const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m,relative_humidity_2m,weather_code&timezone=Asia%2FTokyo&forecast_days=1')
 			
 			// 受け取ったJSON文字列をJavaScriptオブジェクトに変換
 			const data = await response.json()
 
-			// データの中から必要な値を抽出
+			// 現在のhourを取得
 			const hour = dayjs().hour()
-			const temp = data.hourly.temperature_2m[hour - 1]
 
-			return temp
+			// データの中から必要な値を抽出
+			const temp = data.hourly.temperature_2m[hour - 1]
+			const humi = data.hourly.relative_humidity_2m[hour - 1]
+
+			// オブジェクトにまとめる
+			const object = {temp: temp, humi: humi}
+
+			return object
 
 		} catch (error) {
 			
@@ -34,9 +41,6 @@ class WeatherService {
 
 	static async readWeatherInfo(): Promise<WeatherInfo | null> {
 
-		// APIっぽく少し時間をかける
-		await ShuffleService.sleep(400)
-
 		// 現在の日時を取得
 		const dayAndDayOfWeek = dayjs().format("YYYY-MM-DD    dddd")
 		const time = dayjs().format("HH:mm")
@@ -44,10 +48,10 @@ class WeatherService {
 
 
 		// Tokyoの天気を読み取ってみる
-		const tokyoTemp = await this.readTokyoTemp()
-		if (tokyoTemp === null) return null
+		const tokyoWeather = await this.readTokyoWeather()
+		if (tokyoWeather === null) return null
 
-
+		
 
 		// weatherInfoオブジェクトを生成
 		const weatherInfo: WeatherInfo = {
@@ -60,8 +64,8 @@ class WeatherService {
 			nagoyaTemp: 27.2,
 			nagoyaHumi: 61.3,
 			tokyoWeather: "sunny",
-			tokyoTemp: tokyoTemp,
-			tokyoHumi: 42.0,
+			tokyoTemp: tokyoWeather.temp,
+			tokyoHumi: tokyoWeather.humi,
 		}
 
 		return weatherInfo
