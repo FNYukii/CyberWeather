a494fb8062f55b3389c1e96201ed34644ba5477f
FNYukii <forestgreen14689@gmail.com>
2024-07-01 17:37:25 +0900

    feat: 東京の気温をAPIから取得できた

--- src/utils/WeatherService.ts
+++ src/utils/WeatherService.ts
@@ -7,6 +7,30 @@ import WeatherInfo from "../entities/WeatherInfo"
 class WeatherService {
 
 
+	static async readTokyoTemp(): Promise<number | null> {
+
+		try {
+
+			// APIを呼び出す。今回は無料&登録不要の天気API「Open Meteo」を利用
+			const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m&timezone=Asia%2FTokyo&forecast_days=1')
+			
+			// 受け取ったJSON文字列をJavaScriptオブジェクトに変換
+			const data = await response.json()
+
+			// データの中から必要な値を抽出
+			const hour = dayjs().hour()
+			const temp = data.hourly.temperature_2m[hour - 1]
+
+			return temp
+
+		} catch (error) {
+			
+			console.log(`FAIL! Error to call API. ${error}`)
+			return null
+		}
+	}
+
+
 
 	static async readWeatherInfo(): Promise<WeatherInfo | null> {
 
@@ -16,6 +40,14 @@ class WeatherService {
 		// 現在の日時を取得
 		const dayAndDayOfWeek = dayjs().format("YYYY-MM-DD    dddd")
 		const time = dayjs().format("HH:mm")
+
+
+
+		// Tokyoの天気を読み取ってみる
+		const tokyoTemp = await this.readTokyoTemp()
+		if (tokyoTemp === null) return null
+
+
 
 		// weatherInfoオブジェクトを生成
 		const weatherInfo: WeatherInfo = {
@@ -28,7 +60,7 @@ class WeatherService {
 			nagoyaTemp: 27.2,
 			nagoyaHumi: 61.3,
 			tokyoWeather: "sunny",
-			tokyoTemp: 43.1,
+			tokyoTemp: tokyoTemp,
 			tokyoHumi: 42.0,
 		}
 
